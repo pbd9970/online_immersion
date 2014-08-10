26477e1ab98a8ed6bc12b465e0a76501e579ef00
Paul Dombkowski <pdombkowski@gmail.com>
2014-08-10 14:34:19 -0500

    Debugged sign in with Facebook functionality

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,31 +1,50 @@
 class SessionController < ApplicationController
 
   def new
-    redirect_to '/auth/facebook'
+    case params[:provider]
+    when "facebook"
+      redirect_to '/auth/facebook'
+    else
+      flash[:warning] = "Sorry, #{params[:provider].capitalize} is not implemented at this time."
+      redirect_to root_url
+    end
   end
 
   def destroy
     reset_session
-    redirect_to root_url, notice: 'We hope you enjoyed Online Immersion!'
+    flash[:info] = 'We hope you enjoyed Online Immersion!'
+    redirect_to root_url
   end
 
   def create
     auth = request.env["omniauth.auth"]
-    user = User.tokens.where(provider: auth['provider'],
-                      uid: auth['uid'].to_s).first || User.create_with_omniauth(auth)
+    token = get_token(auth, params[:provider])
+
     reset_session
-    session[:user_id] = user.id
-    redirect_to root_url, notice: 'Welcome to Online Immersion!'
+    session[:user_id] = token.user.id
+    flash[:success] = "Logged in as #{token.user.first_name}"
+    redirect_to user_path(token.user)
   end
 
   def failure
-    redirect_to root_url, alert: "Authentication error: #{params[:message].humanize}"
+    flash[:danger] = "Authentication error: #{params[:message].humanize}"
+    redirect_to root_url
   end
-#end
-  def paul
-    user = User.find(1)
-    reset_session
-    session[:user_id] = user.id
-    redirect_to users_path(:id)
+
+  private
+
+  def get_token(auth, provider)
+    # TODO - condense these next two lines? And see about condensing subsequent two lines
+    token = Token.find_by(uid: "#{auth['uid']}", provider: "#{auth['provider']}")
+    token ||= Token.create(uid: "#{auth['uid']}", provider: "#{auth['provider']}")
+    token.token_key = "#{auth['token_key']}"
+    token.token_expiration = "#{auth['token_expiration_date']}"
+    verify_user(token, auth, provider)
+    token.save
+    token
+  end
+
+  def verify_user(token, auth, provider)
+    token.user ||= User.create_with_omniauth(auth, provider)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,27 +2,20 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
   before_filter :correct_user?
 
-  def create
-    @user = User.create(params)
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
   def show
     @user = User.find(params[:id])
-    redirect_to new_chatrooms_path
+    redirect_to new_user_chatroom_path(@user)
   end
 
   def update
     @user = User.update(params)
-    redirect_to new_chatrooms_path
+    redirect_to new_chatroom_path
   end
 
   def destroy
-    
+    @user = User.find(params[:user_id])
+    @user.inactive = true
+    @user.save
+    redirect_to logout_path
   end
 end
