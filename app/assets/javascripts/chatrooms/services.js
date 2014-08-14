db3c6a9222607c5afa3f29bbe4095bfe9c6c46f4
Paul Dombkowski <pdombkowski@gmail.com>
2014-08-14 12:47:42 -0500

    changed io.connect to a different route and it now works with VM configuration

--- app/assets/javascripts/chatrooms/services.js
+++ app/assets/javascripts/chatrooms/services.js
@@ -1,5 +1,5 @@
 app.factory('socket', function ($rootScope) {
-  var socket = io.connect('http://localhost:9000');
+  var socket = io.connect('http://stream.mks.dev:3000');
   return {
     on: function (eventName, callback) {
       socket.on(eventName, function () {
