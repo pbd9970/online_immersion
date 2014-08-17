4856f3264e28711fbbb0a2cfa85fb4bed016c4a1
Paul Dombkowski <pdombkowski@gmail.com>
2014-08-17 15:52:53 -0500

    added ngRoute

--- app/assets/javascripts/chatrooms.js
+++ app/assets/javascripts/chatrooms.js
@@ -1,1 +1,11 @@
-var app = angular.module('chatroom', ['chatroom.filters', 'chatroom.directives']);
+var app = angular.module('chatroom', ['ngRoute', 'chatroom.filters', 'chatroom.directives']);
+
+app.config(function ($routeProvider) {
+
+  $routeProvider.when('/users/:userId/chatrooms/:chatroomId',
+                      {
+                        controller: 'PageController',
+                        templateUrl:'/users/:userId/chatrooms/:chatroomId.html'
+                      })
+                .otherwise({ redirectTo: '/' });
+});
