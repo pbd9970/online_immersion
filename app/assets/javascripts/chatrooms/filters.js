0bfc69d08a1dc9bc7a119397815af428461bb09e
Paul Dombkowski <pdombkowski@gmail.com>
2014-08-19 12:29:29 -0500

    removed files directives, filters, home.js, users.js

--- app/assets/javascripts/chatrooms/filters.js
+++
@@ -1,6 +0,0 @@
-angular.module('chatroom.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
