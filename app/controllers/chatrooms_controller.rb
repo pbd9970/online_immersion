d5507939e645d66b98d46cdc21a63cc55925c62d
Paul Dombkowski <pdombkowski@gmail.com>
2014-08-20 09:28:45 -0500

    added new controller to handle angular requesting it's view from same path plus '/view'

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,6 +2,15 @@ class ChatroomsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :correct_user?
 
+  def index
+    begin
+      @chatroom = Chatroom.find(session[:chatroom_id])
+      render :index
+    rescue
+      redirect_to new_user_chatroom_path(@user)
+    end
+  end
+
   def create
     chat_args = {language_id: params[:language_id].to_i, matched: false, concluded: false}
     cu = :chatroom_users
@@ -9,23 +18,34 @@ class ChatroomsController < ApplicationController
     @chatroom.update(matched: true) if @chatroom.present?
     @chatroom ||= Chatroom.create(chat_args)
     @user.chatrooms << @chatroom
-    redirect_to user_chatroom_path(@user, @chatroom)
+    
+    # Give angular control of application
+    session[:chatroom_id] = @chatroom.id
+    redirect_to "#{user_chatrooms_path(@user)}/#/#{@chatroom.id}"
   end
 
   def new
     @supported_languages = Language.all
   end
 
-  def edit
-  end
-
   def show
-    @chatroom = Chatroom.find(params[:id])
-  end
-
-  def update
+    # Give angular control of application
+    session[:chatroom_id] = params[:id]
+    redirect_to "#{user_chatrooms_path(@user)}/#/#{@chatroom.id}"
   end
 
   def destroy
+    chatroom = Chatroom.find(params[:id])
+    chatroom.update(concluded: true)
+    redirect_to new_user_chatroom_path(@user)
+  end
+
+  def ng_view
+    @chatroom = Chatroom.find(params[:id])
+    if @chatroom
+      render :show, layout: false
+    else
+      redirect_to new_user_chatroom_path(@user)
+    end
   end
 end
